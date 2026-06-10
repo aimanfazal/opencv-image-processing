0694dabd27c46e22ec79e87ce74f7bdf8441d320
Aiman Fazal <twistofaiman@gmail.com>
2026-06-10 14:25:24 +0530

    Added rescaleFrame function

--- face_recognition/face_detect.py
+++ face_recognition/face_detect.py
@@ -1,22 +1,12 @@
 import cv2 as cv
 
-def rescaleFrame(frame, scale = 0.75):
-  # Images, Videos and Live Videos
-  width = int(frame.shape[1] * scale)
-  height = int(frame.shape[0] * scale)
-
-  dimensions = (width, height)
-
-  return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
-
-source = cv.imread('../test_files/5.jpg')
-source = rescaleFrame(source, 0.95)
+source = cv.imread('../media/group_2.jpg')
 gray = cv.cvtColor(source, cv.COLOR_BGR2GRAY)
 
 haar_classifier = cv.CascadeClassifier('../haar_cascade.xml')
 
 #* greater number of minNeighbours, higher accuracy
-face_detect = haar_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+face_detect = haar_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
 
 print(f"Number of face(s) detected: {len(face_detect)}")
 
